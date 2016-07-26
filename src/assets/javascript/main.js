966fb58ba7340bb4b6c81ce27a271852c96aac2f
papaponmx <jaimemadrigalrios@gmail.com>
2016-07-26 12:23:17 -0500

    Removed floats on navigation and used flexbox intstead

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,1 +1,20 @@
 
+// Toggle navigation
+
+function toggle_navigation(element_class) {
+  console.log("Click detected");
+  var e = document.getElementsByClassName(element_class);
+
+  if (e.className == "active") {
+    console.log("Is active");
+
+  }
+
+  else {
+    console.log("Is NOT active");
+    e.classList.remore("trigger");
+    e.classList.add("trigger-active");
+    console.log("Class added");
+  }
+
+}
