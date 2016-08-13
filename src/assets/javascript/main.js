7d78365d9e24080c8b0c235e5608c685ad28ae37
papaponmx <jaimemadrigalrios@gmail.com>
2016-08-13 13:05:13 -0500

    The link to the API works now. The next step is to use the  JSON response from the server

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -18,41 +18,40 @@ function toggleNavigation() {
 
 // Dribbble API interaction
 
-
-
-
-
 $(document).ready(
 
 
   function() {
 
-    var siteTittle = $(document).find("title").text();
+      if ( $('#title__work').length){
+        console.log('It is the work page');
+        console.log('Getting the data from the API :D');
 
-      if (siteTittle == 'work') {
-        console.log('The title is work');
+        $.ajax({
+          type: 'GET',
+          url: 'https://api.dribbble.com/v1/users/papaponmx/shots/?access_token=d0fb37a3cef7a2f82c4b0d13dd9bbc09e36c0bbaf7ca420662a7993846c54af9',
+          success: function(data){
+            console.log('success', data);
+            console.log('Got the data from the API :D');
+
+            // This loops in order to create the gallery
+            for (i = 0; i < 5; i++) {
+
+              console.log('This is an image');
+
+
+              $('#dribbble__gallery').append('<li>' + 'chema'  + '</li>');
+
+            }
+          }
+
+        });
+
       }
-      else {
-        console.log('The site tittle is not work');
-
-      }
-  },
-
-
-
-
-    $.ajax({
-      type: 'GET',
-      url: 'https://api.dribbble.com/v1/users/papaponmx/shots/?access_token=d0fb37a3cef7a2f82c4b0d13dd9bbc09e36c0bbaf7ca420662a7993846c54af9',
-      success: function(data){
-        console.log('success', data);
-        console.log('It worked :D');
-      }
-
-    })
-
-
-
+  }
 );
+
+
+
 
 
